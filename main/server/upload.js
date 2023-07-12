aca4b95b16db855e187bdbb085dbdd91f715bfd6
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-12 20:12:00 +0200

    Created upload

--- main/server/upload.js
+++ main/server/upload.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
+const path = require('path');
 
-function execute(request, response, { middlewareData, extraData: { body } }) {
-    console.log(body)
+const statusCode = require('../functions/error/statusCode.js').execute;
+
+const { code } = require('/secret.json');
+
+function execute(request, response, { extraData: { body } }) {
+    const urlSearchParams = new URLSearchParams(body);
+
+    if (urlSearchParams.get('code') !== code) return statusCode({ request, response, code: 403, text: 'Wrong code' });
+    if (!urlSearchParams.get('fileName')) return statusCode({ request, response, code: 403, text: 'No fileName provided' });
+
+    const baseFilePath = path.join(__dirname, '../../user/files');
+    const filePath = path.join(baseFilePath, urlSearchParams.get('fileName'));
+
+    if (!filePath.startsWith(baseFilePath)) return statusCode({ request, response, code: 403, text: 'Invalid file path' });
+
+    fs.writeFileSync(filePath, body);
 }
 
 module.exports = { execute };
