ea6127a38452c49b4d76b7c1c8979aab8b0ae4ee
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-12 21:04:11 +0200

    Implemented upload

--- files/index.js
+++ files/index.js
@@ -1,3 +1,27 @@
-document.getElementById('file').onchange = function () {
-    document.getElementById('form').submit();
-};
+const file = document.getElementById('file');
+const form = document.getElementById('form');
+const code = document.getElementById('code');
+
+file.addEventListener('change', form.submit());
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const url = new URL(form.action);
+    const formData = new FormData(form);
+
+    url.searchParams.set('code', code.value);
+    url.searchParams.set('fileName', file.files[0].name);
+
+    console.log(url);
+
+    const fetchOptions = {
+        method: form.method,
+        body: formData
+    };
+
+    await fetch(url, fetchOptions);
+
+    console.log('Done')
+
+})
--- main/server/upload.js
+++ main/server/upload.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const statusCode = require('../functions/error/statusCode.js').execute;
 
-const { code } = require('/secret.json');
+const { code } = require('../../secret.json');
 
 function execute(request, response, { extraData: { body } }) {
-    const urlSearchParams = new URLSearchParams(body);
+    const urlSearchParams = new URLSearchParams(request.url.split('?')[1]);
 
     if (urlSearchParams.get('code') !== code) return statusCode({ request, response, code: 403, text: 'Wrong code' });
     if (!urlSearchParams.get('fileName')) return statusCode({ request, response, code: 403, text: 'No fileName provided' });
