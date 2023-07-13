0d7e818938812c2dcc287990b8be947e4edf5f71
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-13 17:08:38 +0200

    Implemented no frames handling

--- webserver/api/getLastFrame.js
+++ webserver/api/getLastFrame.js
@@ -12,7 +12,10 @@ module.exports = {
         if ((!id) && id !== 0) return statusCode(403, 'invalidId', 'Invalid id');
         if (!fs.existsSync(path.join(__dirname, `../../worker/output/${id}/`))) return statusCode(403, 'invalidId', 'Invalid id');
 
-        const filePath = path.join(__dirname, `../../worker/output/${id}/`, fs.readdirSync(path.join(__dirname, `../../worker/output/${id}/`)).sort((a, b) => b.localeCompare(a))[0]);
+        const files = fs.readdirSync(path.join(__dirname, `../../worker/output/${id}/`)).sort((a, b) => b.localeCompare(a));
+        if (files.length === 0) return statusCode(404, 'noFrames', 'The file does not have any frames');
+
+        const filePath = path.join(__dirname, `../../worker/output/${id}/`, files[0]);
 
         response.writeHead(200, { 'Content-Type': 'image/png' });
         fs.createReadStream(filePath).pipe(response);
