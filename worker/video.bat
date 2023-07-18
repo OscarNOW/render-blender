6099d9818f7e719bb33a33f7145a40bdce1db1a5
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-18 22:19:27 +0200

    Fixed bug

--- worker/video.bat
+++ worker/video.bat
@@ -19,7 +19,7 @@ cd output
 cd render
 
 if not exist %1\ (
-    msg "%username%" %1 folder doesn't exist in %cd%
+    msg "%username%" %1 doesn't exist in %cd%
     pause
     exit
 )
@@ -31,15 +31,18 @@ cd ..
 cd ..
 cd ..
 
+if not exist ffmpeg\bin\ (
+    msg "%username%" ffmpeg\bin\ doesn't exist in %cd%
+    pause
+    exit
+)
+
+cd ffmpeg
+cd bin
+
 @REM @REM todo: get fps, amount frames
+ffmpeg -framerate 60 -f image2 -i "%filePath%%%04d.png" -vcodec libx264 -crf 25 -pix_fmt yuv420p -vframes 5 %1.mp4
+move %1.mp4 ..\..\output\video\%1.mp4
 
-@REM if not exist ffmpeg\ (
-@REM     msg "%username%" FFMpeg folder doesn't exist in %cd%
-@REM     pause
-@REM     exit
-@REM )
-
-@REM cd ffmpeg
-@REM ffmpeg -framerate 60 -f image2 -i "%filePath%\%04d.png" -vcodec libx264 -crf 25 -pix_fmt yuv420p -vframes 5 %1.mp4
-@REM move %1.mp4 ..\output\video\%1.mp4
-@REM cd ..
+cd ..
+cd ..
