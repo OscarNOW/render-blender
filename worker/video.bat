68577b456fdfa498db1a574f1d830f781bf78ed6
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-18 22:19:34 +0200

    Changed todo

--- worker/video.bat
+++ worker/video.bat
@@ -40,7 +40,7 @@ if not exist ffmpeg\bin\ (
 cd ffmpeg
 cd bin
 
-@REM @REM todo: get fps, amount frames
+@REM @REM todo-imp: get fps, amount frames
 ffmpeg -framerate 60 -f image2 -i "%filePath%%%04d.png" -vcodec libx264 -crf 25 -pix_fmt yuv420p -vframes 5 %1.mp4
 move %1.mp4 ..\..\output\video\%1.mp4
 
