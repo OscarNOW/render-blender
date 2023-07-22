569ff9171adc488d2d04c136fb6ed15cede737b1
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-22 11:31:52 +0200

    Redirect to new state directly

--- webserver/files/track/handler.js
+++ webserver/files/track/handler.js
@@ -2,7 +2,7 @@ import { getState } from '/js/getState.js';
 import { getInfo } from '/js/getInfo.js';
 
 const { code, id } = getInfo();
-const ourState = window.location.href.split('/track/')[1].split('?')[0].split('/')[0];
+const currentState = window.location.href.split('/track/')[1].split('?')[0].split('/')[0];
 
 const reloadCallbacks = [];
 export function onReload(callback) {
@@ -11,8 +11,8 @@ export function onReload(callback) {
 
 setInterval(reload, 500);
 async function reload() {
-    const currentState = await getState({ code, id });
-    if (currentState !== ourState) return window.location.replace(`/track?id=${id}`);
+    const newState = await getState({ code, id });
+    if (newState !== currentState) return window.location.replace(`/track/${newState}?id=${id}`);
 
     for (const callback of reloadCallbacks)
         await callback();
