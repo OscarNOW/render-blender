ba5f77299ea09c67c1047944fb701d47f3534417
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-22 19:00:14 +0200

    Implemented hasFocus check

--- webserver/files/track/handler.js
+++ webserver/files/track/handler.js
@@ -1,6 +1,8 @@
 import { getState } from '/js/getState.js';
 import { getInfo } from '/js/getInfo.js';
 
+const wait = (ms) => new Promise((res) => setTimeout(res, ms));
+
 const { code, id } = getInfo();
 const currentState = window.location.href.split('/track/')[1].split('?')[0].split('/')[0];
 
@@ -9,7 +11,15 @@ export function onReload(callback) {
     reloadCallbacks.push(callback);
 }
 
-setInterval(reload, 500);
+while (true) {
+    await reload();
+
+    if (document.hasFocus())
+        await wait(500);
+    else
+        await wait(2000);
+}
+
 async function reload() {
     const newState = await getState({ code, id });
     if (newState !== currentState) return window.location.replace(`/track/${newState}?id=${id}`);
