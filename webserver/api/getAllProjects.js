ec2dc4db469513369d4829baa930845df26ff5ec
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-24 21:46:43 +0200

    Implemented getAllProjects

---
+++ webserver/api/getAllProjects.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const { code } = require('../secret.json');
+
+module.exports = {
+    execute({ end, params, statusCode }) {
+        if (params.code !== code) return statusCode(403, 'wrongCode', 'Wrong code');
+        const ids = [];
+
+        for (const checkStage of fs.readdirSync(path.join(__dirname, '../../worker/stages/')))
+            for (const id of fs.readdirSync(path.join(__dirname, `../../worker/stages/${checkStage}/`)))
+                ids.push(id);
+
+        end(JSON.stringify(ids));
+    }
+}
