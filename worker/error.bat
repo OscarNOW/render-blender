4918bee05493ec549b5e7cfea62fd6e58e82c723
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-25 19:22:36 +0200

    Fixed bug in error

--- worker/error.bat
+++ worker/error.bat
@@ -5,6 +5,8 @@ setlocal ENABLEDELAYEDEXPANSION
 @REM %2     project path
 @REM %3     blender path
 
+if exist output\error\%1.txt exit /b 0
+
 set errorMessage=There has been an unknown error
 if exist temp\error.%1.txt set /p errorMessage=< temp\error.%1.txt
 
@@ -12,5 +14,4 @@ echo Error:
 echo !errorMessage!
 
 if not exist output\error\ mkdir output\error\
-
 echo !errorMessage!>output\error\%1.txt
