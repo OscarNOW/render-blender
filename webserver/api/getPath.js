e1ac07fb3200ad9a0d1d543f2411db4129e0f1ab
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-25 14:48:29 +0200

    Implemented getPath

---
+++ webserver/api/getPath.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const { code } = require('../secret.json');
+
+module.exports = {
+    execute({ end, params, statusCode }) {
+        if (params.code !== code) return statusCode(403, 'wrongCode', 'Wrong code');
+
+        const id = params.id;
+
+        if ((!id) && id !== 0) return statusCode(403, 'invalidId', 'Invalid id');
+
+        if (fs.existsSync(path.join(__dirname, '../../worker/stages/')))
+            for (const checkStage of fs.readdirSync(path.join(__dirname, '../../worker/stages/')))
+                for (const checkId of fs.readdirSync(path.join(__dirname, `../../worker/stages/${checkStage}/`)))
+                    if (checkId === id)
+                        return end(fs.readFileSync(path.join(__dirname, `../../worker/stages/${checkStage}/${id}`).toString()));
+
+        return statusCode(403, 'invalidId', 'Invalid id');
+    }
+}
