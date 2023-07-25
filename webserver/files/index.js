23be96d7d9fa40a0b5e9ae3cf88f088230e2e2a3
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-25 14:48:58 +0200

    Changed loading order in loadIds

--- webserver/files/index.js
+++ webserver/files/index.js
@@ -19,10 +19,11 @@ form.addEventListener('submit', async (e) => {
 
 loadIds();
 async function loadIds() {
+    const ids = await fetch(`/api/getAllProjects?code=${code}`).then((resp) => resp.json());
+
     while ([...table.children].length > 1)
         table.removeChild(table.lastChild);
 
-    const ids = await fetch(`/api/getAllProjects?code=${code}`).then((resp) => resp.json());
     for (const id of ids)
         renderId(id);
 }
