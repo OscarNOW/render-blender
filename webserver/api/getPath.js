6d873ad643e8cf3c1d7de471bccf1fd5faefd0dd
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-25 14:53:01 +0200

    Fixed bug in getPath

--- webserver/api/getPath.js
+++ webserver/api/getPath.js
@@ -15,7 +15,7 @@ module.exports = {
             for (const checkStage of fs.readdirSync(path.join(__dirname, '../../worker/stages/')))
                 for (const checkId of fs.readdirSync(path.join(__dirname, `../../worker/stages/${checkStage}/`)))
                     if (checkId === id)
-                        return end(fs.readFileSync(path.join(__dirname, `../../worker/stages/${checkStage}/${id}`).toString()));
+                        return end(fs.readFileSync(path.join(__dirname, `../../worker/stages/${checkStage}/${id}`)).toString());
 
         return statusCode(403, 'invalidId', 'Invalid id');
     }
