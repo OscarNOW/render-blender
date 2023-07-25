a3a254a3594691614f74638b3cfcbeebfdd8f3cb
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-25 18:53:17 +0200

    Fixed bug with temp files in audio

--- worker/audio.bat
+++ worker/audio.bat
@@ -9,19 +9,16 @@ if not exist output\audio\ (
 )
 
 if not exist temp\ mkdir temp\
-if exist temp\temp.%1.py del temp\temp.%1.py
-if exist temp\temp2.%1.py del temp\temp2.%1.py
-
-copy renderAudio.py temp\temp.py
+copy renderAudio.py temp\temp.%1.py
 
 set outputFilePath=%cd%\output\audio\%1.wav
 set escapedOutputFilePath=%outputFilePath:\=\\%
 
 cd temp
-powershell -Command "(gc temp.py) -replace '_outputFilePath_', '%escapedOutputFilePath%' | Out-File -encoding ASCII temp2.py"
+powershell -Command "(gc temp.%1.py) -replace '_outputFilePath_', '%escapedOutputFilePath%' | Out-File -encoding ASCII temp2.%1.py"
 cd ..
 
-%3 -b %2 --python "%cd%\temp\temp2.py" --enable-autoexec
+%3 -b %2 --python "%cd%\temp\temp2.%1.py" --enable-autoexec
 
 del temp\temp.%1.py
 del temp\temp2.%1.py
