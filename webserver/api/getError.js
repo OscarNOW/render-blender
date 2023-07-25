834d2278565c45236d5faa7c291d09d11d8871b4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-07-25 21:29:36 +0200

    Implemented getError

--- webserver/api/getError.js
+++ webserver/api/getError.js
@@ -1,1 +1,21 @@
-//todo-imp
+const fs = require('fs');
+const path = require('path');
+
+const { code } = require('../secret.json');
+
+module.exports = {
+    execute({ end, params, statusCode }) {
+        if (params.code !== code) return statusCode(403, 'wrongCode', 'The code provided is not the correct one');
+
+        const id = params.id;
+        if ((!id) && id !== 0) return statusCode(403, 'invalidId', 'Invalid id');
+
+        const baseFilePath = path.join(__dirname, '../../worker/output/error/');
+        const filePath = path.join(baseFilePath, `/${id}.txt`); //todo: test if the / before ${id} is needed
+
+        if (!filePath.startsWith(baseFilePath)) return statusCode(403, 'invalidId', 'Invalid id');
+        if (!fs.existsSync(filePath)) return statusCode(403, 'invalidId', 'Invalid id');
+
+        end(fs.readFileSync(filePath));
+    }
+}
